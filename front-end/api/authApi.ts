8ddd1ecc40e3fb6cf13fb85cd43426ac8e6b1ad3
A-S-M-Sohag-Abdullah <sohagadullah2021@gmail.com>
2025-06-21 07:39:44 +0600

    Call back urls added

--- front-end/api/authApi.ts
+++ front-end/api/authApi.ts
@@ -23,7 +23,7 @@ export const loginUser = async (credentials: {
 
 // src/redux/api/googleLoginUser.ts
 export const googleLoginUser = async () => {
-  window.open(`https://ecommerce-website-rxvd.onrender.com/api/auth/google/callback`, "_self");
+  window.open(process.env.NEXT_PUBLIC_API_GOOGLE_CALLBACK_URL, "_self");
 };
 
 export const getLoggedInUser = async () => {
